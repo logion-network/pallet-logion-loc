541d1e673c5aeae2d5b01990c0ce831956837fd6
Gérard Dethier <gerard@logion.network>
2022-09-07 11:47:27 +0200

    feat: add seal on close.
    logion-network/logion-internal#570

--- src/migrations.rs
+++ src/migrations.rs
@@ -4,7 +4,61 @@ use frame_support::dispatch::Vec;
 use frame_support::weights::Weight;
 use frame_support::traits::OnRuntimeUpgrade;
 
-use crate::{Config, File, LegalOfficerCaseOf, LocLink, LocMap, LocType, MetadataItem, pallet, PalletStorageVersion, pallet::StorageVersion};
+use crate::{Config, LegalOfficerCaseOf, pallet, PalletStorageVersion, pallet::StorageVersion};
+
+pub mod v8 {
+	use super::*;
+	use crate::*;
+
+	#[derive(Encode, Decode, Default, Clone, PartialEq, Eq, Debug, TypeInfo)]
+	pub struct LegalOfficerCaseV7<AccountId, Hash, LocId, BlockNumber> {
+		owner: AccountId,
+		requester: Requester<AccountId, LocId>,
+		metadata: Vec<MetadataItem<AccountId>>,
+		files: Vec<File<Hash, AccountId>>,
+		closed: bool,
+		loc_type: LocType,
+		links: Vec<LocLink<LocId>>,
+		void_info: Option<LocVoidInfo<LocId>>,
+		replacer_of: Option<LocId>,
+		collection_last_block_submission: Option<BlockNumber>,
+		collection_max_size: Option<CollectionSize>,
+		collection_can_upload: bool,
+	}
+
+	type LegalOfficerCaseOfV7<T> = LegalOfficerCaseV7<<T as frame_system::Config>::AccountId, <T as pallet::Config>::Hash, <T as pallet::Config>::LocId, <T as frame_system::Config>::BlockNumber>;
+
+	pub struct AddSealToLoc<T>(sp_std::marker::PhantomData<T>);
+	impl<T: Config> OnRuntimeUpgrade for AddSealToLoc<T> {
+
+		fn on_runtime_upgrade() -> Weight {
+			super::do_storage_upgrade::<T, _>(
+				StorageVersion::V7ItemToken, 
+				StorageVersion::V8AddSeal, 
+				"AddSealToLoc",
+				|| {
+					LocMap::<T>::translate_values(|loc: LegalOfficerCaseOfV7<T>| {
+						Some(LegalOfficerCaseOf::<T> {
+							owner: loc.owner,
+							requester: loc.requester,
+							metadata: loc.metadata,
+							files: loc.files,
+							closed: loc.closed,
+							loc_type: loc.loc_type,
+							links: loc.links,
+							void_info: loc.void_info,
+							replacer_of: loc.replacer_of,
+							collection_last_block_submission: loc.collection_last_block_submission,
+							collection_max_size: loc.collection_max_size,
+							collection_can_upload: loc.collection_can_upload,
+							seal: Option::None,
+						})
+					})
+				}
+			)
+		}
+	}
+}
 
 pub mod v7 {
 	use super::*;
@@ -58,55 +112,5 @@ where F: FnOnce() -> () {
 			log::warn!("❎ {:?} execution skipped, already at target version {:?}", migration_name, target_version);
 		}
 		0
-	}
-}
-
-pub mod v6 {
-	use crate::{LocVoidInfo, Requester, CollectionSize};
-
-	use super::*;
-
-	#[derive(Encode, Decode, Clone, PartialEq, Eq, Debug)]
-	struct LegalOfficerCaseV5<AccountId, Hash, LocId, BlockNumber> {
-		owner: AccountId,
-		requester: Requester<AccountId, LocId>,
-		metadata: Vec<MetadataItem<AccountId>>,
-		files: Vec<File<Hash, AccountId>>,
-		closed: bool,
-		loc_type: LocType,
-		links: Vec<LocLink<LocId>>,
-		void_info: Option<LocVoidInfo<LocId>>,
-		replacer_of: Option<LocId>,
-		collection_last_block_submission: Option<BlockNumber>,
-		collection_max_size: Option<CollectionSize>,
-	}
-
-	type LegalOfficerCaseOfV5<T> = LegalOfficerCaseV5<<T as frame_system::Config>::AccountId, <T as pallet::Config>::Hash, <T as pallet::Config>::LocId, <T as frame_system::Config>::BlockNumber>;
-
-	pub fn migrate<T: Config>() -> Weight {
-		<LocMap<T>>::translate::<LegalOfficerCaseOfV5<T>, _>(
-			|loc_id: T::LocId, loc: LegalOfficerCaseOfV5<T>| {
-				log::info!("Migrating LOC: {:?}", loc_id);
-				log::info!("From: {:?}", loc);
-				let new_loc = LegalOfficerCaseOf::<T> {
-					owner: loc.owner.clone(),
-					requester: loc.requester.clone(),
-					metadata: loc.metadata.clone(),
-					files: loc.files.clone(),
-					closed: loc.closed.clone(),
-					loc_type: loc.loc_type.clone(),
-					links: loc.links.clone(),
-					void_info: loc.void_info.clone(),
-					replacer_of: loc.replacer_of.clone(),
-					collection_last_block_submission: loc.collection_last_block_submission.clone(),
-					collection_max_size: loc.collection_max_size.clone(),
-					collection_can_upload: false,
-				};
-				log::info!("To: {:?}", new_loc);
-				Some(new_loc)
-			}
-		);
-		let count = <LocMap<T>>::iter().count();
-		T::DbWeight::get().reads_writes(count as Weight + 1, count as Weight + 1)
 	}
 }
